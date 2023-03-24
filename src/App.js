226cb393ac6cd0ba4200b1f6a1936529d9192780
12345432mh <127291858+12345432mh@users.noreply.github.com>
2023-03-24 17:35:51 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header(props){
-  return <header>
-
-  <h1><a href="/" onClick={(event)=>{
-    event.preventDefault();
-    props.onChangeMode();
-  }}>{props.title}</a></h1>
-</header>
-}
-
-function Nav(props){
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
-        event.preventDefault();
-        props.onChangeMode(event.target.id);
-      }}>{t.title}</a></li>)
-  }
-  return <nav>
-  <ol>
-    {lis}
-  </ol>
-</nav>
-}
-
-function Article(props) {
-  return <article>
-  <h2>{props.title}</h2>
-  {props.body}
-  </article>
-}
-
-function App() {
-  const topics = [
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+function App(){
+  const name = '리액트';
   return (
-    <div>
-      <Header title="WEB" onChangeMode={()=>{alert('Header');}}></Header>
-
-      <Nav topics={topics} onChangeMode={(id)=>{alert(id);}}></Nav>
-      
-      <Article title="Welcome" body="Hello, WEB"></Article>
-    
-    </div>
+    <>
+    {/*주석은 이렇게 작성합니다 */}
+    <div className="react">
+    {name} </div>
+    // 하지만 이런 주석이나
+    /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+    <input />
+    </>
   );
 }
 
